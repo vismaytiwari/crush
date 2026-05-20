3d82c8eea679772e501e74b954e9f8ea1f0653b2
Kieran Klukas <kieran@dunkirk.sh>
2026-05-20 17:52:52 -0400

    feat: add custom prompt paths

--- internal/agent/agent.go
+++ internal/agent/agent.go
@@ -150,11 +150,12 @@ type Model struct {
 }
 
 type sessionAgent struct {
-	largeModel         *csync.Value[Model]
-	smallModel         *csync.Value[Model]
-	systemPromptPrefix *csync.Value[string]
-	systemPrompt       *csync.Value[string]
-	tools              *csync.Slice[fantasy.AgentTool]
+	largeModel          *csync.Value[Model]
+	smallModel          *csync.Value[Model]
+	systemPromptPrefix  *csync.Value[string]
+	systemPrompt        *csync.Value[string]
+	tools               *csync.Slice[fantasy.AgentTool]
+	customSummaryPrompt string
 
 	isSubAgent           bool
 	sessions             session.Service
@@ -210,6 +211,7 @@ type SessionAgentOptions struct {
 	SmallModel           Model
 	SystemPromptPrefix   string
 	SystemPrompt         string
+	CustomSummaryPrompt  string
 	IsSubAgent           bool
 	DisableAutoSummarize bool
 	IsYolo               bool
@@ -233,6 +235,7 @@ func NewSessionAgent(
 		messages:             opts.Messages,
 		disableAutoSummarize: opts.DisableAutoSummarize,
 		tools:                csync.NewSliceFrom(opts.Tools),
+		customSummaryPrompt:  opts.CustomSummaryPrompt,
 		isYolo:               opts.IsYolo,
 		notify:               opts.Notify,
 		runComplete:          opts.RunComplete,
@@ -1312,9 +1315,13 @@ func (a *sessionAgent) Summarize(ctx context.Context, sessionID string, opts fan
 		}
 	}()
 
+	sysPrompt := string(summaryPrompt)
+	if a.customSummaryPrompt != "" {
+		sysPrompt = a.customSummaryPrompt
+	}
 	agent := fantasy.NewAgent(
 		largeModel.Model,
-		fantasy.WithSystemPrompt(string(summaryPrompt)),
+		fantasy.WithSystemPrompt(sysPrompt),
 		fantasy.WithUserAgent(userAgent),
 	)
 	summaryMessage, err := a.messages.Create(ctx, sessionID, message.CreateMessageParams{
